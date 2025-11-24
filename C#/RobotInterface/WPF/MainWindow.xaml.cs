c1d8e8a8213198845af9439362d01445d800aa31
iutgeiitoulonE105 <touloniut@gmail.com>
2025-11-24 12:31:19 +0100

    ROBOT1
    Avancement C#

--- C#/RobotInterface/WPF/MainWindow.xaml.cs
+++ C#/RobotInterface/WPF/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Shapes;
 using ExtendedSerialPort_NS;
 using System.IO.Ports;
 using System.Runtime.CompilerServices;
+using System.Windows.Threading;
 
 namespace WPF
 {
@@ -20,16 +21,43 @@ namespace WPF
     public partial class MainWindow : Window
     {
         ExtendedSerialPort serialPort1;
+        DispatcherTimer timerAffichage;
+        Robot robot = new Robot();
+
         public MainWindow()
         {
             /*serialPort1 = new ExtendedSerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
             serialPort1.Open();
             InitializeComponent();
             */
-            serialPort1 = new ReliableSerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+            serialPort1 = new ExtendedSerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
             serialPort1.DataReceived += SerialPort1_DataReceived;
             serialPort1.Open();
+
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick;
+            timerAffichage.Start();
         }
+
+        private void TimerAffichage_Tick(object? sender, EventArgs e)
+        {
+            if (receivedText != "")
+            {
+                textBoxReception.Text += receivedText;
+                receivedText = "";
+            }
+        }
+
+        string receivedText;
+        private void SerialPort1_DataReceived(object? sender, DataReceivedArgs e)
+        {
+            //throw new NotImplementedException();
+
+            receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+
+        }
+
         int backChange = 0;
         private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
         {
@@ -57,11 +85,16 @@ namespace WPF
             string message = textBoxEmission.Text;
             if (!string.IsNullOrWhiteSpace(message))
             {
-                RichTextBox.AppendText(Environment.NewLine + "Reçu : " + message);
+                //RichTextBox.AppendText(Environment.NewLine + "Reçu : " + message);
                 textBoxEmission.Clear();
             }
             serialPort1.Write(message);
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxReception.Clear();
         }
     }
 }
